4be370c31d888c86830194ed0dad4070bdbf66e4
ishikavats52 <sishika077@gmail.com>
2026-07-30 12:14:48 +0530

    ui fixes

--- src/screens/main/MISScreen.tsx
+++ src/screens/main/MISScreen.tsx
@@ -8,7 +8,7 @@ import apiClient from '../../api/client';
 import { Ionicons } from '@expo/vector-icons';
 import * as Print from 'expo-print';
 import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 
 type Props = {
   navigation: NativeStackNavigationProp<MainStackParamList, 'MISReport'>;
